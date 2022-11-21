e1c827623405f953e6a8ece4012f7f4f8a7f0eb4
shkiperoO <44169697+shkiperoO@users.noreply.github.com>
2022-11-21 16:14:22 +0000

    Meals component logic

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,5 +1,20 @@
+import { useGlobalContext } from '../context'
 const Meals = () => {
-    return <h1>Meals Component</h1>
+
+  const{meals} = useGlobalContext()
+    return <section className='section-center'>
+      {meals.map((singleMeal) => {
+      const {idMeal, strMeal:title, strMealThumb:image} = singleMeal
+        return <article key={idMeal} className="single-meal">
+                <img src={image} classNeme="img" style={{width: '200px'}}/>
+                  <footer>
+                    <h5>{title}</h5>
+                    <button className='like-btn'>click me</button>
+                  </footer>
+               </article>
+      
+      })}
+    </section>
 }
 
 export default Meals
--- src/context.jsx
+++ src/context.jsx
@@ -1,14 +1,37 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
+import axios from 'axios'
 
 const AppContext = React.createContext()
 
+const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
+const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 const AppProvider = ({ children }) => {
+  const [meals,setMeals] = useState([])
 
-    return <AppContext.Provider value='hello'>
+  
+  const fetchMeals = async(url) =>{
+    
+    try {
+      const {data} = await axios(url)
+      setMeals(data.meals)
+      
+    }catch(error){
+      console.log(error.response)
+    }
+    
+  }
+
+useEffect(()=>{
+  fetchMeals(allMealsUrl)
+},[])
+  
+    return <AppContext.Provider value={{meals}}>
         {children}
     </AppContext.Provider>
 
 }
-
+export const useGlobalContext = () => {
+  return useContext(AppContext)
+}
 export {AppContext, AppProvider}
