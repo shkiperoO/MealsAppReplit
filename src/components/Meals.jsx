f4a79bfebf75fa11e0b1ee669a8f3df66d834bd3
shkiperoO <44169697+shkiperoO@users.noreply.github.com>
2022-11-21 19:15:53 +0000

    errors handling

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -3,7 +3,19 @@ import { BsHandThumbsUp } from 'react-icons/bs'
 
  const Meals = () => {
 
-  const{meals} = useGlobalContext()
+  const{meals, loading} = useGlobalContext()
+
+   if(loading){
+     return <section className="section">
+        <h4>Loading...</h4>
+     </section>
+   }
+  if(meals.length < 1){
+     return <section className="section">
+        <h4>No meals matched your search term. Please try again.</h4>
+     </section>
+  }
+   
     return <section className='section-center'>
       {meals.map((singleMeal) => {
       const {idMeal, strMeal:title, strMealThumb:image} = singleMeal
--- src/context.jsx
+++ src/context.jsx
@@ -7,18 +7,26 @@ const allMealsUrl = 'https://www.themealdb.com/api/json/v1/1/search.php?s=a'
 const randomMealUrl = 'https://www.themealdb.com/api/json/v1/1/random.php'
 
 const AppProvider = ({ children }) => {
+  const [loading,setLoading] = useState(false)
   const [meals,setMeals] = useState([])
 
   
   const fetchMeals = async(url) =>{
-    
+    setLoading(true)
     try {
       const {data} = await axios(url)
-      setMeals(data.meals)
+
+      if(data.meals){
+        setMeals(data.meals)
+      }else{
+        setMeals([])
+      }
+      
       
     }catch(error){
       console.log(error.response)
     }
+    setLoading(false)
     
   }
 
@@ -26,7 +34,7 @@ useEffect(()=>{
   fetchMeals(allMealsUrl)
 },[])
   
-    return <AppContext.Provider value={{meals}}>
+    return <AppContext.Provider value={{loading, meals}}>
         {children}
     </AppContext.Provider>
 
